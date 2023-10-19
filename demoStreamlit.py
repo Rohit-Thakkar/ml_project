055610abee340736c7177e48c9b67145ccdd41a3
Rohit Thakkar <97794321+Rohit-Thakkar@users.noreply.github.com>
2023-10-19 21:00:21 +0530

    Update demoStreamlit.py

--- demoStreamlit.py
+++ demoStreamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
